7d99f632246293cd60de36ea64be429837dd9d77
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-01-28 23:02:42 -0300

    teste de busca de tópico

--- src/test/kotlin/br/com/alura/forum/service/TopicoServiceTest.kt
+++ src/test/kotlin/br/com/alura/forum/service/TopicoServiceTest.kt
@@ -3,15 +3,20 @@ package br.com.alura.forum.service
 import br.com.alura.forum.mapper.TopicoFormMapper
 import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.TopicoTest
+import br.com.alura.forum.model.TopicoViewTest
 import br.com.alura.forum.repository.TopicoRepository
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
 import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 
 class TopicoServiceTest {
 
     private val topicos = PageImpl(listOf(TopicoTest.build()))
 
+    private val paginacao: Pageable = mockk()
     private val repository: TopicoRepository = mockk {
         every { findByCursoNome(any(), any()) } returns topicos
     }
@@ -21,4 +26,12 @@ class TopicoServiceTest {
     val topicoService = TopicoService(repository, topicoViewMapper, topicoFormMapper)
 
 
+    @Test
+    fun `deve listar topicos a partir do nome do curso`() {
+        every { topicoViewMapper.map(any()) } returns TopicoViewTest.build()
+        topicoService.listar("Kotlin avançado", paginacao)
+        verify(exactly = 1) { repository.findByCursoNome(any(), any()) }
+        verify(exactly = 1) { topicoViewMapper.map(any()) }
+        verify(exactly = 0) { repository.findAll(paginacao) }
+    }
 }
