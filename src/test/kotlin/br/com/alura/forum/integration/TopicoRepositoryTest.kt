1de038f4af9a0924de68bfcde2e5ead09d4301a1
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-01-30 22:51:49 -0300

    configuração do testcontainer

--- src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
+++ src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
@@ -2,12 +2,37 @@ package br.com.alura.forum.integration
 
 import br.com.alura.forum.repository.TopicoRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TopicoRepositoryTest {
 
     @Autowired
     private lateinit var topicoRepository: TopicoRepository
 
+    companion object {
+        @Container
+        private val mysqlContainer = MySQLContainer<Nothing>("mysql:8.0").apply {
+            withDatabaseName("testdb")
+            withUsername("teste")
+            withPassword("123456")
+        }
 
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url") { mysqlContainer.jdbcUrl }
+            registry.add("spring.datasource.password") { mysqlContainer.password }
+            registry.add("spring.datasource.username") { mysqlContainer.username }
+        }
 
+    }
 }
