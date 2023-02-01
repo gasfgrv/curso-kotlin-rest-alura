1dcdfe30e58e407b2602b269f7c435fcf596b01f
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-02-01 20:11:26 -0300

    Relatório com Thymeleaf

--- src/main/kotlin/br/com/alura/forum/controller/RelatorioController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/RelatorioController.kt
@@ -2,17 +2,21 @@ package br.com.alura.forum.controller
 
 import br.com.alura.forum.dto.TopicoPorCategoriaDto
 import br.com.alura.forum.service.TopicoService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("relatorios")
 class RelatorioController(
         private val topicoService: TopicoService
 ) {
 
     @GetMapping
-    fun relatorio(): List<TopicoPorCategoriaDto> = topicoService.relatorio()
+    fun relatorio(model: Model): String {
+        model.addAttribute("topicosPorCategorias", topicoService.relatorio())
+        return "relatorio"
+    }
 
 }
