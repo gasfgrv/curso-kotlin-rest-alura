45e70d06ddd7b2db516ff28920197eb66d24ea97
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-02-01 21:09:12 -0300

    testcontainer do redis

--- src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
+++ src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -33,12 +34,20 @@ class TopicoRepositoryTest {
             withPassword("123456")
         }
 
+        @Container
+        private val redisContainer = GenericContainer<Nothing>("redis:latest").apply {
+            withExposedPorts(6379)
+        }
+
         @JvmStatic
         @DynamicPropertySource
         fun properties(registry: DynamicPropertyRegistry) {
             registry.add("spring.datasource.url") { mysqlContainer.jdbcUrl }
             registry.add("spring.datasource.password") { mysqlContainer.password }
             registry.add("spring.datasource.username") { mysqlContainer.username }
+
+            registry.add("spring.redis.host") { redisContainer.containerIpAddress }
+            registry.add("spring.redis.port") { redisContainer.firstMappedPort }
         }
     }
 
